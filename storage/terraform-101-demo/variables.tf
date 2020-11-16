f2972c136b1468816304049278872534e85cd5c7
Matthew Fortunka <mafortun@microsoft.com>
2020-11-16 10:24:48 +0000

    added demo module

---
+++ storage/terraform-101-demo/variables.tf
@@ -0,0 +1,11 @@
+variable "storage_account_name" {
+    description = "The storage account name"
+} 
+
+variable "resource_group_name" {
+    description = "The resource group name"
+} 
+
+variable "location" {
+    description = "The location to deploy resources"
+} 
